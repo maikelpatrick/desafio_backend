e7ab070278903bfcc64dcacb2ffbf689fb8e04ca
maikelpatrick <maikelpatrick@gmail.com>
2023-01-17 19:30:56 -0300

    commit 2

--- src/main/java/com/desafio/resouces/AgenteResurces.java
+++ src/main/java/com/desafio/resouces/AgenteResurces.java
@@ -8,11 +8,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,19 +32,18 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 @RequestMapping(value = "/desafio")
 public class AgenteResurces {
 	
-	@Autowired
 	private AgenteService service;
-	
-	@Autowired
 	private RegiaoService serviceRegiao;
-		
-	@Autowired
 	private GeracaoService serviceGeracao;
-	
-	@Autowired
 	private CompraService serviceCompra;
-	
-	
+
+	public AgenteResurces(AgenteService service, RegiaoService serviceRegiao, GeracaoService serviceGeracao, CompraService serviceCompra) {
+		this.service = service;
+		this.serviceRegiao = serviceRegiao;
+		this.serviceGeracao = serviceGeracao;
+		this.serviceCompra = serviceCompra;
+	}
+
 	@GetMapping
 	public ResponseEntity<List<GeracaoDTO>> findAll(){
 		List<GeracaoDTO> list = serviceGeracao.findAll().stream().map(obj -> new GeracaoDTO(obj)).collect(Collectors.toList());
@@ -56,92 +51,87 @@ public class AgenteResurces {
 	}
 	
 	@PostMapping
-	public ResponseEntity<AgenteDTO> create( MultipartFile[] obj) throws IOException{ 
-		 int cont = 0;		 
-		if(obj.length>0) {
-		 for(int i = 0; i < obj.length; i++) {
-			 XmlMapper xmlMapper = new XmlMapper();
-			 
-			 JsonNode node = xmlMapper.readTree(obj[i].getBytes());
-			 
-			 JsonNode root1 = node.path("agente");
-			 if (!root1.isArray()) { 
-				 AgenteDTO agenteDTO = new AgenteDTO();
-				 agenteDTO.setCodigo(root1.path("codigo").asText());
-				 agenteDTO.setData(root1.path("data").asText());                 
-				 
-				 Agente adto = new Agente(service.create(agenteDTO));			
-				 System.out.println("Agente código : " + adto.getCodigo());	
-				 
-				 for(JsonNode reg : root1.path("regiao")) {
-					 RegiaoDTO regDTO = new RegiaoDTO();
-					 regDTO.setSigla(reg.path("sigla").asText());
-					 regDTO.setAgente(adto);
-					 
-					 Regiao regiaoSave = new Regiao(serviceRegiao.create(regDTO));
-					 
-					 int j = 0;
-					 for(JsonNode gera : reg.path("geracao")) {
-						 
-							 GeracaoDTO geraDTO = new GeracaoDTO();
-							 geraDTO.setValor(gera.get(j).asText());
-							 geraDTO.setRegiao(regiaoSave);
-							 Geracao geracao = new Geracao(serviceGeracao.create(geraDTO) );
-							 j++;
-					 }
-					 int b =0;
-					 for(JsonNode compra : reg.path("compra")) {
-						 CompraDTO comDTO = new CompraDTO();
-						 comDTO.setCompra(compra.get(b).asText());	
-						 comDTO.setRegiao(regiaoSave);
-						Compra compraRetorno = new Compra(serviceCompra.create(comDTO));
+	public ResponseEntity<AgenteDTO> create( MultipartFile[] obj) throws IOException{
+		int cont = 0;
 
-						  b++;
+		if (obj != null) {
+			for (int i = 0; i < obj.length; i++) {
+				XmlMapper xmlMapper = new XmlMapper();
+				JsonNode node = xmlMapper.readTree(obj[i].getBytes());
+				JsonNode root1 = node.path("agente");
+
+				if (!root1.isArray()) {
+					AgenteDTO agenteDTO = new AgenteDTO();
+					agenteDTO.setCodigo(root1.path("codigo").asText());
+					agenteDTO.setData(root1.path("data").asText());
+
+					Agente adto = service.create(agenteDTO);
+
+					for (JsonNode reg : root1.path("regiao")) {
+						RegiaoDTO regDTO = new RegiaoDTO();
+						regDTO.setSigla(reg.path("sigla").asText());
+						regDTO.setAgente(adto);
+
+						Regiao regiaoSave = serviceRegiao.create(regDTO);
+						JsonNode nodesGeracao = reg.path("geracao").get("valor");
+
+						for (JsonNode gera : nodesGeracao) {
+							GeracaoDTO geraDTO = new GeracaoDTO();
+							geraDTO.setValor(gera.asText());
+							geraDTO.setRegiao(regiaoSave);
+							serviceGeracao.create(geraDTO);
 						}
-					 }
-					 cont++;
-				 
-			 }else {
-				 
-				 for (JsonNode root : node.path("agente")) {
-					 AgenteDTO agenteDTO = new AgenteDTO();
-					 agenteDTO.setCodigo(root.path("codigo").asText());
-					 agenteDTO.setData(root.path("data").asText());                 
-					 
-					 Agente adto = new Agente(service.create(agenteDTO));			
-					 System.out.println("Agente código : " + adto.getCodigo());			
-					 
-					 for(JsonNode reg : root.path("regiao")) {
-						 RegiaoDTO regDTO = new RegiaoDTO();
-						 regDTO.setSigla(reg.path("sigla").asText());
-						 regDTO.setAgente(adto);
-						 
-						 Regiao regiaoSave = new Regiao(serviceRegiao.create(regDTO));
-						 
-						 int j = 0;
-						 for(JsonNode gera : reg.path("geracao")) {
-							 
-								 GeracaoDTO geraDTO = new GeracaoDTO();
-								 geraDTO.setValor(gera.get(j).asText());
-								 geraDTO.setRegiao(regiaoSave);
-								 Geracao geracao = new Geracao(serviceGeracao.create(geraDTO) );
-								 j++;
-						 }
-						 int b = 0;
-						 for(JsonNode compra : reg.path("compra")) {
-							 CompraDTO comDTO = new CompraDTO();
-							 comDTO.setCompra(compra.get(b).asText());	
-							 comDTO.setRegiao(regiaoSave);
-							 Compra compraRetorno = new Compra(serviceCompra.create(comDTO));
 
-							  b++;
+						JsonNode nodesCompra = reg.path("compra").get("valor");
+
+						for (JsonNode compra : nodesCompra) {
+							CompraDTO comDTO = new CompraDTO();
+							comDTO.setCompra(compra.asText());
+							comDTO.setRegiao(regiaoSave);
+							serviceCompra.create(comDTO);
+						}
+					}
+
+					cont++;
+				} else {
+					for (JsonNode root : node.path("agente")) {
+						AgenteDTO agenteDTO = new AgenteDTO();
+						agenteDTO.setCodigo(root.path("codigo").asText());
+						agenteDTO.setData(root.path("data").asText());
+
+						Agente adto = service.create(agenteDTO);
+
+						for (JsonNode reg : root.path("regiao")) {
+							RegiaoDTO regDTO = new RegiaoDTO();
+							regDTO.setSigla(reg.path("sigla").asText());
+							regDTO.setAgente(adto);
+
+							Regiao regiaoSave = serviceRegiao.create(regDTO);
+							JsonNode nodesGeracao = reg.path("geracao").get("valor");
+
+							for (JsonNode gera : nodesGeracao) {
+								GeracaoDTO geraDTO = new GeracaoDTO();
+								geraDTO.setValor(gera.asText());
+								geraDTO.setRegiao(regiaoSave);
+								serviceGeracao.create(geraDTO);
 							}
-						 }
-						 cont++;
-					 }
-				 }
-			 }
+
+							JsonNode nodesCompra = reg.path("compra").get("valor");
+
+							for (JsonNode compra : nodesCompra) {
+								CompraDTO comDTO = new CompraDTO();
+								comDTO.setCompra(compra.asText());
+								comDTO.setRegiao(regiaoSave);
+								serviceCompra.create(comDTO);
+							}
+						}
+
+						cont++;
+					}
+				}
+			}
 		}
+
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cont).toUri();
 		return ResponseEntity.created(uri).build();
 	}
